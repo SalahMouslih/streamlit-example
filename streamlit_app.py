de4a8737739690878cd8f8b0556faef8bf8f53db
Salah Moslih <62819403+SalahMouslih@users.noreply.github.com>
2023-09-26 15:37:47 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,5 +14,19 @@ Refe to [documentation](https://docs.streamlit.io) of the app to understand app 
 """
 
 
-#uploaded_file = st.file_uploader("Upload your images here...")
+import pickle
+import streamlit as st
+import pandas as pd
 
+# load pre-trained model
+trained_model = pickle.load(open(model_filename, 'rb'))
+
+uploaded_file = st.file_uploader("Upload your file here...", type=['csv'])
+
+if uploaded_file is not None:
+    dataframe = pd.read_csv(uploaded_file)
+
+	X = dataframe[["column1","column2"]]
+	result = trained_model.predict(X)
+
+	st.write(f"Your prediction is: {result}")
