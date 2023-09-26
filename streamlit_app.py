24ca4e8bcbb2a7fd49519ba879167b79d302b742
Salah Moslih <62819403+SalahMouslih@users.noreply.github.com>
2023-09-26 17:12:31 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -20,6 +20,54 @@ Refe to [documentation](https://docs.streamlit.io) of the app to understand app 
 ### Classify bags
 """
 
+def transform(mode):
+    """
+    Returns an *unfitted* transformer that defines ``fit()`` and ``transform()`` methods.
+    The transformer's input and output signatures should be compatible with scikit-learn
+    transformers.
+    """
+
+    from transformers import ViTImageProcessor
+    from torchvision import transforms
+
+    feature_extractor = ViTImageProcessor.from_pretrained("google/vit-base-patch16-224-in21k")
+
+    image_mean, image_std = feature_extractor.image_mean, feature_extractor.image_std
+    size = feature_extractor.size["height"]
+    normalize = transforms.Normalize(mean=image_mean, std=image_std)
+
+    _train_transforms = transforms.Compose(
+        [   
+            transforms.RandomResizedCrop(size),
+            transforms.RandomHorizontalFlip(),
+            transforms.ToTensor(),
+            normalize,
+        ]
+    )
+
+    _val_transforms = transforms.Compose(
+        [
+            transforms.Resize(size),
+            transforms.CenterCrop(size),
+            transforms.ToTensor(),
+            normalize,
+        ]
+    )
+
+    _test_transforms = transforms.Compose(
+        [
+            transforms.Resize(size),
+            transforms.CenterCrop(size),
+            transforms.ToTensor(),
+            normalize,
+        ]
+    )
+    
+    transforms =  {'train': _train_transforms, 'val': _val_transforms, 'test': _test_transforms}
+
+    return transforms[mode]
+
+
 # load pre-trained model
 trained_model = torch.load('./model/data/model.pth',map_location=torch.device('cpu') )
 
