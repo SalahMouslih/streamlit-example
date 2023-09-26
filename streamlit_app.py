d9c1a12bcaa495f3f75cee4996ff49beb1b27750
Salah Moslih <62819403+SalahMouslih@users.noreply.github.com>
2023-09-26 15:47:30 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,6 +4,7 @@ import math
 import pandas as pd
 import streamlit as st
 import pandas as pd
+import mlflow
 
 """
 # Welcome to Afraudet!
@@ -22,4 +23,14 @@ Refe to [documentation](https://docs.streamlit.io) of the app to understand app 
 """
 uploaded_files = st.file_uploader("Upload your files here...", accept_multiple_files=True)
 
+import mlflow
+logged_model = 'runs:/3c802963efed4b84be2eea9ccb96be8a/pytorch-tuned-EFF_NET-model'
 
+# Load model as a PyFuncModel.
+loaded_model = mlflow.pyfunc.load_model(logged_model)
+
+if loaded_model:
+  print('success')
+else : print('nothing')
+# Predict on a Pandas DataFrame.
+#loaded_model.predict(pd.DataFrame(data))
